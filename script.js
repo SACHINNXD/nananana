27eb582fe78e2cd8a0b88fd8a3a47bb166efe975
SACHINNXD <145748982+SACHINNXD@users.noreply.github.com>
2026-02-04 10:46:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+const openBtn = document.getElementById("openSearch");
+const closeBtn = document.getElementById("closeSearch");
+const overlay = document.getElementById("searchOverlay");
 
+openBtn.addEventListener("click", () => {
+    overlay.classList.add("active");
+});
+
+closeBtn.addEventListener("click", () => {
+    overlay.classList.remove("active");
+});
