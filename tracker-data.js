a390abb47cd4637410d2a63ef2e6dd7e6f69ca23
SACHINNXD <145748982+SACHINNXD@users.noreply.github.com>
2026-02-04 12:09:54 +0530

    Create tracker-data.js

---
+++ tracker-data.js
@@ -0,0 +1,15 @@
+// tracker-data.js
+// This simulates your private database
+
+const trackerData = [
+    {
+        orderId: "CR71001",
+        email: "user1@example.com",
+        mobile: "9876543210"
+    },
+    {
+        orderId: "CR71002",
+        email: "user2@example.com",
+        mobile: "9123456789"
+    }
+];
